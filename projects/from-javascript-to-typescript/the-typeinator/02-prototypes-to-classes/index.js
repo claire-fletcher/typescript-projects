3912df353a04e0c9f731801339644a44f95226be
Claire Fletcher <claire.fletcher@kaluza.com>
2024-12-23 11:45:26 +0000

    feat(typeinator): Solution 2 with tests passing

--- projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
@@ -1,5 +1,59 @@
 // Put your Robot and Humanoid classes here! ✨
 // See ./original.js for their older JavaScript code.
 
+// A JavaScript class is not an object.
+// It is a template for JavaScript objects.
+// constructor is needed and auto run when object created to initialise things
+// Methods specific to the class are also defined within the class
+// then when the object is created you have access to those.
+
+class Robot {
+	constructor(name, abilities) {
+		this.name = name;
+		this.abilities = abilities;
+		this.power = 100;
+	}
+
+	announce() {
+		console.log(`Greetings. I am ${this.name}.`);
+
+		for (var i = 0; i < this.abilities.length; i += 1) {
+			console.log(`I am able to ${this.abilities[i]}.`);
+		}
+	}
+
+	charge(amount) {
+		if (this.power < 100) {
+			this.power = Math.min(this.power + amount, 100);
+			console.log(`Recharged power supplies to ${this.power}.`);
+		}
+
+		if (this.power === 100) {
+			console.log("I am at optimal operational capacity.");
+		}
+	}
+
+	move(distance) {
+		if (this.power < distance) {
+			console.log(`I do not have enough power to move ${distance} units.`);
+		} else {
+			console.log(`Moving ${distance} units.`);
+			this.power -= distance;
+		}
+	}
+}
+
+class Humanoid extends Robot {
+	constructor(name, abilities, catchphrase) {
+		super(name, abilities);
+		this.catchphrase = catchphrase;
+	}
+
+	announce() {
+		super.announce();
+		console.log(`> ${this.catchphrase} <`);
+	}
+}
+
 module.exports.Humanoid = Humanoid;
 module.exports.Robot = Robot;
