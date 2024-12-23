7b806d5b7de467c68049e68450e54fc71e44586c
Claire Fletcher <claire.fletcher@kaluza.com>
2024-12-23 12:14:56 +0000

    feat(typinator): solution 3 with passing tests. more research into async and promise and await in javascript.

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,22 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+async function checkEmotion(knownEmotions, emotion) {
+	// waits for 1000 to happen before doing the knext atep.
+	await new Promise((resolve) => {
+		return setTimeout(resolve, 1000);
+	});
+
+	return knownEmotions.has(emotion);
+}
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	if (!(await checkEmotion(knownEmotions, newEmotion))) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
